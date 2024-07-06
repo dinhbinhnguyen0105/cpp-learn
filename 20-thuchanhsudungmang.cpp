9c47d176184f358e5a26e079c4e00ab089be519a
dinhbinhnguyen0105 <dinhbinhnguyen0105@gmail.com>
2024-07-06 16:43:47 +0700

    from macmini

--- 20-thuchanhsudungmang.cpp
+++ 20-thuchanhsudungmang.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
 int main() {
-    int n;
-    std::cout << "Nhap so nguyen duong < 10: "; std::cin >> n;
-    int numbers[n];
-    int x;
-    std::cout << "Nhap so nguyen duong x: "; std::cin >> x;
-    numbers[n - 1] = x;
 
-    // for(int i = 0; i < sizeof())
-
-    std::cout << numbers[n-1] << std::endl;
-    std::cout << sizeof(numbers);
-
+    std::cout << "Bai 1:" << std::endl;
+    {
+        int n;
+        std::cout << "Nhap so nguyen duong < 10: "; std::cin >> n;
+        int numbers[n];
+        int x;
+        std::cout << "Nhap so nguyen duong x: "; std::cin >> x;
+        numbers[n - 1] = x;
+        std::cout << numbers[n-1] << std::endl;
+    }
+    std::cout << "Bai 2:" << std::endl;
+     
+    
     return 0;
 }
